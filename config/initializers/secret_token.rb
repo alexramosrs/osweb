ef6837268912136f2260334fd58d7a1bd505eac8
leandrocm <leandro@microcel.com.br>
2012-06-16 15:48:48 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osweb::Application.config.secret_token = '005d5363baf7fee05194cd2bf31cd8b7cdd667416b148a552c9299777d02a2a3e67df4379399c5fcc8b20674530eb651139baef3d95b8b45487b8442c64a90a8'
